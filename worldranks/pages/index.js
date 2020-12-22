ed806ad99d9cdc13a84b65c186e59e29973b0012
Khushi <dpskhu13108@gmail.com>
2020-12-22 18:34:13 +0530

    added components

--- worldranks/pages/index.js
+++ worldranks/pages/index.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
-import Layout from '../components/Layout';
+import Layout from '../components/Layout/Layout';
+import styles from "../styles/Home.module.css";
+import SearchInput from "../components/search/search"
 
-export default function Home() {
+export default function Home({countries}) {
+  console.log(countries);
   return (
-    <Layout>main</Layout>
+    <Layout>
+      <div className={styles.counts}> Found {countries.length} Countries</div>
+      <SearchInput/>
+      <CountriesTable countries={filteredCountries} />
+    </Layout>
   )
 }
+
+export const getStaticProps = async() => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
